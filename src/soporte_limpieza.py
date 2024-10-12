6487a856df46600db3e716544cd50d2e487393cb
elenacano <elena.canocas@gmail.com>
2024-10-12 20:22:14 +0200

    paso1 fase 2

--- src/soporte_limpieza.py
+++ src/soporte_limpieza.py
@@ -1,4 +1,14 @@
-import pandas as ps
+import pandas as pd
 
 def suma(a,b):
     return a+b
+
+
+def rellenar_id_os(id, nombre, dic, df):
+    if dic[nombre] != id:
+        print(f"El id es {id} y deberia ser {dic[nombre]}")
+
+def rellenar_os(id, nombre, dic, df):
+    if dic[id] != nombre :
+        df["nombre organizacion"] = dic[id]
+        
