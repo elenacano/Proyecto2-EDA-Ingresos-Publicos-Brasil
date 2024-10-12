7275a31d987af40931cceeffbf47ce6f3989d48b
elenacano <elena.canocas@gmail.com>
2024-10-12 22:47:01 +0200

    fase2 parte2

--- src/soporte_limpieza.py
+++ src/soporte_limpieza.py
@@ -12,3 +12,27 @@ def rellenar_os(id, nombre, dic, df):
     if dic[id] != nombre :
         df["nombre organizacion"] = dic[id]
         
+
+def cambio_id_39251(id):
+    if id == 39251:
+        return 68201
+    else:
+        return id
+    
+def cambio_id_20117(id):
+    if id == 20117:
+        return 30912
+    else:
+        return id
+    
+def cambio_id_64902(id):
+    if id == 64902:
+        return 30914
+    else:
+        return id
+    
+def cambio_id_64901(id):
+    if id == 64901:
+        return 30913
+    else:
+        return id
